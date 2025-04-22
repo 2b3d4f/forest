65e20acf9d386fa1890932a4b543c1f2f8d630ec
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-04-23 00:20:24 +0900

    update add-remark-toc plugin

--- plugins/add_remark_toc.ts
+++ plugins/add_remark_toc.ts
@@ -12,24 +12,60 @@ export const defaults: Options = {
   text: "## Table of Contents",
 };
 
-function replaceText(content: string, userOptions: Options): string {
+function replacePlaceholder(content: string, userOptions: Options): string {
   const options = merge(defaults, userOptions);
   const tocHeader = `${options.text}`;
   return content.replace(options.placeholder, tocHeader);
 }
 
+function toValidHtmlId(input: string): string {
+  if (!input.trim()) return "id";
+
+  return input
+    .normalize("NFKD")
+    .replace(/\p{Cc}/gu, "")
+    .replace(/\s+/g, "-")
+    .replace(/[^a-zA-Z0-9\-_:.]/g, "")
+    .replace(/^-+|-+$/g, "")
+    .toLowerCase();
+}
+
 export function addRemarkToc(userOptions: Options) {
   const options = merge(defaults, userOptions);
-
   return (site: Site) => {
     site.preprocess([".md"], (pages) => {
       for (const page of pages) {
         try {
-          page.data.content = replaceText(page.data.content as string, options);
+          page.data.content = replacePlaceholder(
+            page.data.content as string,
+            options
+          );
         } catch (error) {
           log.error(
             `[add-remark-toc plugin] Error replacing ${page.sourcePath}: ${error}`
           );
+        }
+      }
+    });
+    site.process([".html"], (pages) => {
+      for (const page of pages) {
+        try {
+          for (const tocHeader of page.document.querySelectorAll(
+            `.prose > #${toValidHtmlId(options.text.trim())}`
+          )) {
+            const wrapper = page.document.createElement("div");
+            const sibling = tocHeader.nextElementSibling;
+
+            wrapper.classList.add("table-of-contents");
+            tocHeader.parentNode?.insertBefore(wrapper, tocHeader);
+
+            wrapper.appendChild(tocHeader);
+            if (sibling) {
+              wrapper.appendChild(sibling);
+            }
+          }
+        } catch (error) {
+          log.error(`[add-remark-toc plugin] Error wrapping ToC in ${page.sourcePath}: ${error}`)
         }
       }
     });
