c2a553065fc740a2c9ea6c1a0c616ef18d63a1e1
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-04-23 12:43:22 +0900

    disable git data plugin

--- _config.ts
+++ _config.ts
@@ -1,19 +1,23 @@
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
 
+import gitData from "./plugins/git_data.ts";
+
 const site = lume({
   src: "./src",
 });
 
-site.use(
-  plugins({
-    prism: {
-      theme: {
-        name: "okaidia",
-        cssFile: "prism.css",
+site
+  .use(
+    plugins({
+      prism: {
+        theme: {
+          name: "okaidia",
+          cssFile: "prism.css",
+        },
       },
-    },
-  })
-);
+    })
+  )
+  .use(gitData());
 
 export default site;
--- plugins.ts
+++ plugins.ts
@@ -1,4 +1,3 @@
-import gitData from "./plugins/git_data.ts";
 import {
   googleFonts,
   Options as GoogleFontsOptions,
@@ -117,7 +116,6 @@ export default function (userOptions?: Options) {
     site.data("fonts", options.fonts);
 
     site
-      .use(gitData())
       .use(addRemarkToc(options.addToc))
       .use(
         googleFonts({
