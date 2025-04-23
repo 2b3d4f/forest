ce1de6eb1d80e5c97eefbe4afdd17c592f59e509
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-04-23 14:45:32 +0900

    revert recent changes

--- _config.ts
+++ _config.ts
@@ -1,10 +1,22 @@
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
+import gitData from "./plugins/git_data.ts";
 
 const site = lume({
   src: "./src",
 });
 
-site.use(plugins());
+site
+  .use(
+    plugins({
+      prism: {
+        theme: {
+          name: "okaidia",
+          cssFile: "prism.css",
+        },
+      },
+    })
+  )
+  .use(gitData());
 
 export default site;
