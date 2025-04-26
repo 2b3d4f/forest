28bd7619cf4484e66fe65fcf997bd402bde5037a
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-04-26 10:17:33 +0900

    update test

--- test/_config.ts
+++ test/_config.ts
@@ -1,8 +1,34 @@
 import lume from "lume/mod.ts";
 import theme from "theme/mod.ts";
 
+import gitData from "../plugins/git_data.ts";
+
 const site = lume();
 
-site.use(theme());
+site
+  .use(
+    theme({
+      googleFonts: {
+        fonts: {
+          display:
+            "https://fonts.google.com/share?selection.family=Libre+Baskerville:ital,wght@0,400;0,700;1,400",
+          text: "https://fonts.google.com/share?selection.family=Gentium+Book+Plus:ital,wght@0,400;0,700;1,400;1,700",
+        },
+      },
+      fonts: {
+        fallback: {
+          display: "serif",
+          text: "serif",
+        },
+      },
+      prism: {
+        theme: {
+          name: "okaidia",
+          cssFile: "prism.css",
+        },
+      },
+    })
+  )
+  .use(gitData());
 
 export default site;
