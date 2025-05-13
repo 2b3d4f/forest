6fbe593d530404cb321ac15e63fd1070fb84e66e
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-05-13 11:29:13 +0900

    update plugin

--- plugins/repo_latest_hash.ts
+++ plugins/repo_latest_hash.ts
@@ -1,16 +1,13 @@
 import Site from "lume/core/site.ts";
-import { Extensions } from "lume/core/utils/path.ts";
 import { getRepoLatestCommitHash } from "../utils/git.ts";
 import { merge } from "lume/core/utils/object.ts";
 
 export interface Options {
-  extensions?: Extensions;
   hashesKey?: string;
   repoKey?: string;
 }
 
 export const defaults: Options = {
-  extensions: [".html"],
   hashesKey: "hashes",
   repoKey: "repo",
 };
@@ -21,14 +18,9 @@ export const defaults: Options = {
 export function repoLatestHash(userOptions?: Options) {
   const options = merge(defaults, userOptions);
   return (site: Site) => {
+    site.mergeKey(options.hashesKey, "object");
     const hash = getRepoLatestCommitHash();
-    site.preprocess(options.extensions, (pages) => {
-      for (const page of pages) {
-        page.data[options.hashesKey] ??= {};
-
-        if (hash) page.data[options.hashesKey][options.repoKey] ??= hash.long;
-      }
-    });
+    site.data(options.hashesKey, { [options.repoKey]: hash?.long });
   };
 }
 
